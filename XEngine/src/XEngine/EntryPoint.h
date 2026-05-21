6e4e42c71f79757411fa74c8e664bf019538d555
Qi <Qi@PC>
2026-05-21 19:58:52 +0800

    完成了Log系统的初步开发和premake引入，一键创建工程文件

--- XEngine/src/XEngine/EntryPoint.h
+++ XEngine/src/XEngine/EntryPoint.h
@@ -6,6 +6,11 @@ extern XEngine::Application* XEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	XEngine::Log::Init();
+	X_CORE_WARN("Initialized Log!");
+	int a = 5;
+	X_INFO("Hello! Var={0}", a);
+
 	auto app = XEngine::CreateApplication();
 	app->Run();
 	delete app;
